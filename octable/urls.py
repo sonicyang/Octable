ab608acaff79c1cb46a2faa9b3d5db33ad2098d3
Sonic Yang <sonic.tw.tp@gmail.com>
2015-02-05 15:21:23 +0800

    fix static folder

--- octable/urls.py
+++ octable/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from octable.views import index
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'octable.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', index),
 
     url(r'^admin/', include(admin.site.urls)),
 )
