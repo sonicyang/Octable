972715178bcfbd201724acb7132a147de43f7ef7
sonicyang <sonic.tw.tp@gmail.com>
2015-02-06 00:14:59 +0800

    Add code to pasrse TIME

--- octable/database/fetch.py
+++ octable/database/fetch.py
@@ -24,7 +24,7 @@ def main():
     for e in departmentList:
         print("Updating Department : " + e.select("a")[0]["href"][-2:])
         try:
-            cur.execute("CREATE TABLE " + e.select("a")[0]["href"][-2:] + "(ID INT, SerialNum INT, CourseNum TEXT, ClassNum TEXT, Class INT, Grade INT, TYPE TEXT, GRP INT, ENG INT, Name TEXT, Optional INT, Credits INT, Prof TEXT, Selected INT, Free INT, Day INT, HourStart INT, HourEnd INT, Place TEXT, Memo TEXT, Restriction TEXT, Industrial INT, Property TEXT, Moocs INT)")
+            cur.execute("CREATE TABLE " + e.select("a")[0]["href"][-2:] + "(ID INT, SerialNum INT, CourseNum TEXT, ClassNum TEXT, Class INT, Grade INT, TYPE TEXT, GRP INT, ENG INT, Name TEXT, Optional INT, Credits INT, Prof TEXT, Selected INT, Free INT, MON INT, TUE INT, WED INT, THS INT, FRI INT, SAT INT, Place TEXT, Memo TEXT, Restriction TEXT, Industrial INT, Property TEXT, Moocs INT)")
             print("Table for Department " + e.select("a")[0]["href"][-2:] + " Created!")
         except:
             print("Table for Department " + e.select("a")[0]["href"][-2:] + " already Exist!")
@@ -50,7 +50,7 @@ def main():
             for c in courses:
                 cols = c.select('td')
 
-                sqlcode = "INSERT INTO " + e.select("a")[0]["href"][-2:] + "(SerialNum, CourseNum, ClassNum, Class, Grade, TYPE, GRP, ENG, Name, Optional, Credits, Prof, Selected, Free, Day, HourStart, HourEnd, Place, Memo, Restriction, Industrial, Property, Moocs) VALUES("
+                sqlcode = "INSERT INTO " + e.select("a")[0]["href"][-2:] + "(SerialNum, CourseNum, ClassNum, Class, Grade, TYPE, GRP, ENG, Name, Optional, Credits, Prof, Selected, Free, MON, TUE, WED, THS, FRI, SAT, Place, Memo, Restriction, Industrial, Property, Moocs) VALUES("
 
                 print("SerialNum : " + cols[2].getText())
                 sqlcode += cols[2].getText() + ","
@@ -143,25 +143,107 @@ def main():
                 print("Free : " + cols[15].getText())
                 sqlcode += cols[15].getText() + ","
 
-                print("Time : " + cols[16].getText())
-                tmp = 0
-                if "[1]" in cols[16].getText():
-                    tmp += 1
-                if "[2]" in cols[16].getText():
-                    tmp += 2
-                if "[3]" in cols[16].getText():
-                    tmp += 4
-                if "[4]" in cols[16].getText():
-                    tmp += 8
-                if "[5]" in cols[16].getText():
-                    tmp += 16
-                if "[6]" in cols[16].getText():
-                    tmp += 32
-                if tmp == 0:
-                    """raise"""
-                sqlcode += str(tmp) + ","
 
-                sqlcode += cols[16].getText() + "," """Need to select Date"""
+                dayData = [0, 0, 0, 0, 0, 0]
+                sFlag = 0
+                DayHolder = 0
+                LinkHolder = 0
+                LastReaded = 0
+                print("Time : " + cols[16].getText())
+                for j in range(0, len(cols[16].getText())):
+                    if cols[16].getText()[j] == "[" and not sFlag:
+                        sFlag = 1
+                        DayHolder = 0
+                    elif cols[16].getText()[j] == "]" and sFlag:
+                        sFlag = 0
+                    elif sFlag:
+                        DayHolder = int(cols[16].getText()[j])
+                    elif cols[16].getText()[j] == "~":
+                        LinkHolder = 1
+                    elif not sFlag:
+                        if not LinkHolder:
+                            if cols[16].getText()[j] == "1":
+                                dayData[DayHolder - 1] += 1
+                                LastReaded = 1
+                            elif cols[16].getText()[j] == "2":
+                                dayData[DayHolder - 1] += 2
+                                LastReaded = 2
+                            elif cols[16].getText()[j] == "3":
+                                dayData[DayHolder - 1] += 4
+                                LastReaded = 3
+                            elif cols[16].getText()[j] == "4":
+                                dayData[DayHolder - 1] += 8
+                                LastReaded = 4
+                            elif cols[16].getText()[j] == "N":
+                                dayData[DayHolder - 1] += 16
+                                LastReaded = 5
+                            elif cols[16].getText()[j] == "5":
+                                dayData[DayHolder - 1] += 32
+                                LastReaded = 6
+                            elif cols[16].getText()[j] == "6":
+                                dayData[DayHolder - 1] += 64
+                                LastReaded = 7
+                            elif cols[16].getText()[j] == "7":
+                                dayData[DayHolder - 1] += 128
+                                LastReaded = 8
+                            elif cols[16].getText()[j] == "8":
+                                dayData[DayHolder - 1] += 256
+                                LastReaded = 9
+                            elif cols[16].getText()[j] == "9":
+                                dayData[DayHolder - 1] += 512
+                                LastReaded = 10
+                            elif cols[16].getText()[j] == "A":
+                                dayData[DayHolder - 1] += 1024
+                                LastReaded = 11
+                            elif cols[16].getText()[j] == "B":
+                                dayData[DayHolder - 1] += 2048
+                                LastReaded = 12
+                            elif cols[16].getText()[j] == "C":
+                                dayData[DayHolder - 1] += 4096
+                                LastReaded = 13
+                            elif cols[16].getText()[j] == "D":
+                                dayData[DayHolder - 1] += 8192
+                                LastReaded = 14
+                        else:
+                            tmp = 0
+                            if cols[16].getText()[j] == "2":
+                                tmp = 2
+                            elif cols[16].getText()[j] == "3":
+                                tmp = 3
+                            elif cols[16].getText()[j] == "4":
+                                tmp = 4
+                            elif cols[16].getText()[j] == "N":
+                                tmp = 5
+                            elif cols[16].getText()[j] == "5":
+                                tmp = 6
+                            elif cols[16].getText()[j] == "6":
+                                tmp = 7
+                            elif cols[16].getText()[j] == "7":
+                                tmp = 8
+                            elif cols[16].getText()[j] == "8":
+                                tmp = 9
+                            elif cols[16].getText()[j] == "9":
+                                tmp = 10
+                            elif cols[16].getText()[j] == "A":
+                                tmp = 11
+                            elif cols[16].getText()[j] == "B":
+                                tmp = 12
+                            elif cols[16].getText()[j] == "C":
+                                tmp = 13
+                            elif cols[16].getText()[j] == "D":
+                                tmp = 14
+                            for l in range(LastReaded + 1, tmp + 1):
+                                dayData[DayHolder - 1] += 2 ** (l - 1)
+                            LinkHolder = 0
+                    else:
+                        raise
+
+                sqlcode += str(dayData[0]) + ","
+                sqlcode += str(dayData[1]) + ","
+                sqlcode += str(dayData[2]) + ","
+                sqlcode += str(dayData[3]) + ","
+                sqlcode += str(dayData[4]) + ","
+                sqlcode += str(dayData[5]) + ","
 
                 if len(cols[17].select("a")) > 0:
                     print("Place : " + cols[17].select("a")[0].getText())
